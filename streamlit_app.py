564e3e3c89e0e4e7be71f5270808d1ec2e5d57b6
Matthew Ludwig <mats51@web.de>
2025-01-03 20:39:10 +0000

    remove key

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,14 +7,15 @@ if "pill1" not in st.session_state:
 if "pill2" not in st.session_state:
     st.session_state.pill2 = "Option 2"
 
+# This doesnt work
 st.session_state.pill1 = st.pills(
     "Pills 1 with default randomly resets after selection",
     options=pills_options,
     selection_mode="single",
-    key="public_selection",
     default=st.session_state.pill1
 )
 
+# This works
 st.session_state.pill2 = st.pills(
     "Pills w/o default works",
     options=pills_options,
